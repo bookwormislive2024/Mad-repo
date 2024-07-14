f5d8dd296b0c0e7c45c7c9b77edfbf774013d1b1
bookwormislive2024 <infohubstore03@gmail.com>
2024-07-14 21:06:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,17 +17,17 @@ print(f'Public IP Address: {ip}')
 load_dotenv()
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "28277294"))
-API_HASH = os.environ.get("API_HASH", "7ec1420b010830ad701cf518ce0474580")
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "0")
 
 
-OWNER = os.environ.get("OWNER", "@ULTIMATE_GUARDIAN_X")  # Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", "1768198143"))  # Owner user id
+OWNER = os.environ.get("OWNER", "")  # Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Owner user id
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002122561727"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL1 = int(os.environ.get(
     "FORCE_SUB_CHANNEL1", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get(
@@ -43,7 +43,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "3"))
 
 # start message
 START_MSG = os.environ.get(
-    "START_MESSAGE", "🌸 ᴀʀɪɢᴀᴛᴏᴜ, {first}!\n\n 🌸 ᴇᴍʙʀᴀᴄᴇ ᴛʜᴇ ᴀɴɪᴍᴇ ᴍᴀɢɪᴄ! ʜᴇʀᴇ, ᴘɪxᴇʟꜱ ᴅᴀɴᴄᴇ, ꜱᴛᴏʀɪᴇꜱ ᴜɴꜰᴏʟᴅ, ᴀɴᴅ ᴇᴍᴏᴛɪᴏɴꜱ ɪɢɴɪᴛᴇ. ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ꜱᴀɴᴄᴛᴜᴀʀʏ—ᴀ ᴘʟᴀᴄᴇ ᴡʜᴇʀᴇ ᴀɴɪᴍᴇ ᴅʀᴇᴀᴍꜱ ᴛᴀᴋᴇ ꜰʟɪɢʜᴛ. ᴇɴᴊᴏʏ ᴛʜᴇ ᴊᴏᴜʀɴᴇʏ! 🌟")
+    "START_MESSAGE", "")
 
 try:
     ADMINS = [1768198143]
@@ -54,7 +54,7 @@ except ValueError:
 
 
 FORCE_MSG = os.environ.get(
-    "FORCE_SUB_MESSAGE", "<b><center> 🌸 𝙆𝙤𝙣𝙣𝙞𝙘𝙝𝙞𝙬𝙖, 𝙛𝙚𝙡𝙡𝙤𝙬 𝙤𝙩𝙖𝙠𝙪! 🌸 </center>\n\n 𝙄𝙛 𝙮𝙤𝙪’𝙧𝙚 𝙖 𝙩𝙧𝙪𝙚 𝙨𝙚𝙣𝙥𝙖𝙞, 𝙟𝙤𝙞𝙣 𝙢𝙮 𝙘𝙝𝙖𝙣𝙣𝙚𝙡𝙨 𝙖𝙣𝙙 𝙡𝙚𝙩’𝙨 𝙬𝙚𝙖𝙫𝙚 𝙤𝙪𝙧 𝙖𝙣𝙞𝙢𝙚 𝙢𝙖𝙜𝙞𝙘 𝙩𝙤𝙜𝙚𝙩𝙝𝙚𝙧! 🎉 𝙇𝙚𝙩’𝙨 𝙨𝙥𝙧𝙞𝙣𝙠𝙡𝙚 𝙖 𝙡𝙞𝙩𝙩𝙡𝙚 “𝙠𝙖𝙬𝙖𝙞𝙞” 𝙞𝙣𝙩𝙤 𝙤𝙪𝙧 𝙘𝙤𝙣𝙫𝙚𝙧𝙨𝙖𝙩𝙞𝙤𝙣𝙨, 𝙗𝙚𝙘𝙖𝙪𝙨𝙚 𝙡𝙞𝙛𝙚 𝙞𝙨 𝙗𝙚𝙩𝙩𝙚𝙧 𝙬𝙞𝙩𝙝 𝙖 𝙩𝙤𝙪𝙘𝙝 𝙤𝙛 𝙅𝙖𝙥𝙖𝙣𝙚𝙨𝙚 𝙛𝙡𝙖𝙞𝙧. 🌟🇯🇵</b>")
+    "FORCE_SUB_MESSAGE", "")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
