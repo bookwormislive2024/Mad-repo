97e7ea0978b0c82a9cf94a3c57c501bd5b4ca621
bookwormislive2024 <infohubstore03@gmail.com>
2024-07-14 21:29:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,21 +17,21 @@ print(f'Public IP Address: {ip}')
 load_dotenv()
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "0")
+APP_ID = int(os.environ.get("APP_ID", "21145186"))
+API_HASH = os.environ.get("API_HASH", "daa53f4216112ad22b8a8f6299936a46")
 
 
-OWNER = os.environ.get("OWNER", "")  # Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Owner user id
+OWNER = os.environ.get("OWNER", "6011680723")  # Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", "@the_universal_being"))  # Owner user id
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002159492954"))
 FORCE_SUB_CHANNEL1 = int(os.environ.get(
-    "FORCE_SUB_CHANNEL1", "0"))
+    "FORCE_SUB_CHANNEL1", "-1002123546604"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get(
-    "FORCE_SUB_CHANNEL2", "0"))
+    "FORCE_SUB_CHANNEL2", "-1002170049626"))
 
 
 SECONDS = int(os.getenv("SECONDS", "600"))  # auto delete in seconds
@@ -43,11 +43,11 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "3"))
 
 # start message
 START_MSG = os.environ.get(
-    "START_MESSAGE", "")
+    "START_MESSAGE", "ʜᴇʟʟᴏ ᴛʜᴇʀᴇ!!{mention}👋\n\nɪ ᴀᴍ ᴀ ꜰɪʟᴇ ꜱʜᴀʀᴇ ʙᴏᴛ ᴜꜱᴇᴅ ᴀɴᴅ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʏ ᴍᴀꜱᴛᴇʀ - ɪɴꜰᴏʜᴜʙ ɴᴇᴛᴡᴏʀᴋꜱ 💫")
 
 try:
-    ADMINS = [1768198143]
-    for x in (os.environ.get("ADMINS", "1768198143 6450266465 7065803173").split()):
+    ADMINS = [6011680723]
+    for x in (os.environ.get("ADMINS", "6011680723 1524169222 2007123863").split()):
         ADMINS.append(int(x))
 except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
@@ -65,10 +65,10 @@ DISABLE_CHANNEL_BUTTON = os.environ.get(
     "DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌ʙᴀᴋᴀᴀ! ᴅᴏɴ'ᴛ ꜱᴇɴᴅ ᴍᴇ ᴍᴇꜱꜱᴀɢᴇ"
+USER_REPLY_TEXT = "❌ ᴡʜʏ ɪɴ ᴛʜᴇ ᴡᴏʀʟᴅ ᴡᴏᴜʟᴅ ʏᴏᴜ ᴛᴇxᴛ ᴍᴇ?💀 ɪ'ᴍ ᴊᴜꜱᴛ ᴀ ꜰɪʟᴇ ꜱʜᴀʀᴇ ʙᴏᴛ ʙᴜᴅᴅʏ! ❌"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1768198143)
+ADMINS.append(6011680723)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
